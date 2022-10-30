27c7245fbcbd29bfd1dcc789edf36b5aabb26bbf
AvniErtas <avniertas@outlook.com>
2022-10-30 19:54:52 +0300

    first mongodbexample

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:mongodbexample/dbHelper/show.dart';
 import 'package:mongodbexample/mongodb.dart';
 
 // https://www.youtube.com/watch?v=yJtfID3iS0g&list=PL6KvyFEnJSRYjyZi7xOh3v_pc0-Q7jmuA&index=3
-
+ //
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MongoDatabase.connect();
